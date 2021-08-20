9276ba125dadfb29efc70b3121c953580616e0b8
Hal Star <halstar@hotmail.com>
2021-08-20 17:23:17 +0200

    Use center aligned counter mode for PWM as it's the recommended method

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -662,7 +662,7 @@ static void MX_TIM8_Init(void)
   /* USER CODE END TIM8_Init 1 */
   htim8.Instance = TIM8;
   htim8.Init.Prescaler = 7;
-  htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim8.Init.CounterMode = TIM_COUNTERMODE_CENTERALIGNED1;
   htim8.Init.Period = 100;
   htim8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim8.Init.RepetitionCounter = 0;
